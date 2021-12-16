0b4519fc4715f4257bb0f9a53c6a042d7960ab39
Kirill Mironov <kirill.mironov@gmail.com>
2021-12-16 09:57:25 +0300

    initial commit

---
+++ bin/quadratic_solver.dart
@@ -0,0 +1,5 @@
+import 'package:quadratic_solver/quadratic_solver.dart' as quadratic_solver;
+
+void main(List<String> arguments) {
+  print('Hello world: ${quadratic_solver.calculate()}!');
+}
---
+++ test/quadratic_solver_test.dart
@@ -0,0 +1,8 @@
+import 'package:quadratic_solver/quadratic_solver.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
